edf32f7adafe84e0edbb7a88c730dbf1563b6198
Phill Barron <phillipbarron242@gmail.com>
2019-08-14 16:38:55 +0100

    made a  start on script

--- host-os.sh
+++ host-os.sh
@@ -2,8 +2,10 @@
 
 if [ "$(uname)" == "Darwin" ]
 then
+    echo "MacOS land"
     export HOST_OS="MACOS"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Linux land"
     export HOST_OS="LINUX"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     echo "Windows not currently supported"
--- setup-profile.sh
+++ setup-profile.sh
@@ -3,9 +3,26 @@
 # Export host OS
 source $PWD/host-os.sh
 
+installForLinux() {
+    # Install ZSH and Oh-My-ZSH
+    apt-get install -y zsh curl git
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
+
+installForMacOS() {
+    # Install ZSH and Oh-My-ZSH
+    brew install zsh curl git
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
+
 if [ -d "$HOME/.profile_config" ]
 then 
     echo "looks like it's already installed"
 else
-    echo "LETS INSTALL ALL THE THINGS"
+    if [ $HOST_OS == "LINUX" ]
+    then
+        installForLinux
+    else
+        installForMacOS
+    fi
 fi
