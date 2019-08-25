b40388dda410f47e3d35ddd9a2a0fbbeb2252625
Phill Barron <phillipbarron242@gmail.com>
2019-08-25 14:34:22 +0100

    moved all the sti=uff about and added script to intstall it all

--- README.md
+++ README.md
@@ -1,20 +1,13 @@
 # profile
 
-this is the bash profile for the Mac - this will mostly work in ubuntu too but may need a little tweaking.
-
-all the config files are in .profile_config and will need copying in to the home folder:
+Profile setup script - installs zsh & on-my-zsh then adds some other stuff.
 
 ```bash
-mkdir $HOME/.profile_config
-
-cp .profile_config/.* $HOME/.profile_config
-cp .zshrc $HOME/.zshrc
-
-init
+./setuo-profile.sh
 ```
 
 ### todo
 
 * create a variables file for all the secret stuff and reference throughout
 * integrate with [profile stuff](https://github.com/phillipbarron/dev-env)
-* make sure this works both on the mac and linux
+* make sure this works both on the mac and linux (in progress)
--- setup-profile.sh
+++ setup-profile.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 
 # Export host OS
-source $PWD/host-os.sh
+. $PWD/host-os.sh
+
+
+copyConfigfiles(){
+    mkdir $HOME/.profile_config
+    cp dotfiles/* $HOME/.profile_config
+    mv $HOME/.zshrc $HOME/.zshrc.bak
+    mv $HOME/.profile_config/zshrc $HOME/.zshrc
+}
 
 installForLinux() {
     # Install ZSH and Oh-My-ZSH
     apt-get install -y zsh curl git
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    copyConfigfiles
 }
 
 installForMacOS() {
     # Install ZSH and Oh-My-ZSH
     brew install zsh curl git
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    copyConfigfiles
 }
+
 
 if [ -d "$HOME/.profile_config" ]
 then 
