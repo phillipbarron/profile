6bbbb2d3d43ad5ab1b2dea060846e3c2b5715180
Phill Barron <phillipbarron242@gmail.com>
2021-05-24 10:43:08 +0100

    make java cert ca bundle work for mac & linux (well ubuntu at least)

--- certificates/cert_generation_java.sh
+++ certificates/cert_generation_java.sh
@@ -3,6 +3,15 @@
 # TODO: Does this work with Java 9?
 # Java Keystore w/BBC CA Certificates
 
-cp /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts $HOME/.certs/jssecacerts
+# Export host OS
+. $PWD/host-os.sh
+
+
+if [ $HOST_OS == "LINUX" ]; then
+    cp /etc/ssl/certs/java/cacerts $HOME/.certs/jssecacerts
+else
+    cp /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts $HOME/.certs/jssecacerts
+fi
+
 keytool -import -trustcacerts -file $HOME/.certs/ca.pem -alias bbc-ca -keystore $HOME/.certs/jssecacerts -storepass changeit -noprompt
 keytool -import -trustcacerts -file $HOME/.certs/cloud-ca.pem -alias bbc-cloud-ca -keystore $HOME/.certs/jssecacerts -storepass changeit -noprompt
